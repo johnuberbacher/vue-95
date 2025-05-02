59b91f1ba45b16d2d4b3fd81b113064feaa37660
John Uberbacher <johnuberbacher@gmail.com>
2025-05-01 21:05:48 -0600

    updates

--- src/main.js
+++ src/main.js
@@ -2,9 +2,6 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
-import '@/assets/scss/custom.scss'
-import '@/assets/scss/variables.scss'
-
 const app = createApp(App)
 
 app.use(createPinia())
--- src/stores/interface.js
+++ src/stores/interface.js
@@ -2,7 +2,7 @@ import { defineStore } from "pinia";
 
 export const useInterfaceStore = defineStore("interface", {
   state: () => ({
-    fullscreenMode: false,
+    fullscreenMode: true,
     crtMode: true,
     programContextMenuActive: false, 
     desktopContextMenuActive: false, 
